2a77f4558031b64637901f0b1506c234d096bffb
DBX12 <dbx12@dbx12.de>
2021-10-27 07:52:52 +0200

    Add missing cd

--- src/bitbutler.sh
+++ src/bitbutler.sh
@@ -800,6 +800,7 @@ function selfupdate() {
   # This file cannot be followed but also does not add important information for the script.
   #shellcheck disable=SC1090
   source "${BB_VENDOR_PATH}/make.settings"
+  cd "${unpacked_tar}"
   make -f "${unpacked_tar}/Makefile" install
 }
 
